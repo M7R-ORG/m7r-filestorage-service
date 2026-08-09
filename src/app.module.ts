e3247f62132d32582df12f8da06ce2323a5ae77c
SiAvMaAnR <siavmaanr@gmail.com>
2026-08-09 21:04:59 +0300

    Add health check endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/app.config';
 import { FilesModule } from './files/files.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { FilesModule } from './files/files.module';
       load: [config],
     }),
     FilesModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
